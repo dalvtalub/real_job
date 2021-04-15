dad35e71e1ce822c5b559f0c3b84d32273b46575
dalvtalub <vlad.bulat.65@mail.ru>
2021-04-15 22:25:02 +0300

    changed max_length

--- authentication/models.py
+++ authentication/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class MyToken(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
-    mytoken = models.CharField('Token', max_length=40)
+    mytoken = models.CharField('Token', max_length=200)
 
     def __str__(self):
         return f'{self.user} {self.mytoken}'
