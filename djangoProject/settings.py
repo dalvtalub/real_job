ec2d6b5b12d7cbe5a527a7a2a7a2ec6f09e80fe2
dalvtalub <vlad.bulat.65@mail.ru>
2021-04-16 17:47:18 +0300

    details

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -139,5 +139,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = MEDIA_DIR
+# MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
