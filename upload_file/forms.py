3043ac8b3d58f8e3582051254c05b7d8e9583e65
dalvtalub <vlad.bulat.65@mail.ru>
2021-04-16 16:37:18 +0300

    details

--- upload_file/forms.py
+++ upload_file/forms.py
@@ -6,4 +6,3 @@ class CsvForm(forms.ModelForm):
     class Meta:
         model = Csv
         fields = ('file_name',)
-        # widgets = {'file_name': forms.FileField(attrs={'accept': '.csv'}),}
--- upload_file/views.py
+++ upload_file/views.py
@@ -7,17 +7,17 @@ from .models import Csv
 import csv
 
 
-# from django.contrib.auth.decorators import login_required
-#
-#
-# @login_required
 def upload_file(request):
     form = CsvForm(request.POST or None, request.FILES or None)
-    session = request.session
-    # print(session.get())
     if form.is_valid():
-        form.save()  # after completing the addition - go to the galvanized page
-        form = CsvForm()
+        filename = (str(request.FILES['file_name'])).split('.')
+
+        if not filename[-1] == 'csv':
+            return render(request, 'upload_file.html',
+                          {'form': CsvForm(), 'information': 'Invalid type of file, choose '
+                                                             '.csv file'})
+
+        form.save()
         obj = Csv.objects.get(activated=False)
         with open(obj.file_name.path) as f:
             reader = csv.reader(f)
@@ -27,16 +27,12 @@ def upload_file(request):
                 author_name_and_surname = row[1]
                 name_of_book = row[2]
                 year_of_writing = int(row[3])
-                if Authors.objects.filter(date_of_birth=author_birth, name_and_surname=author_name_and_surname).exists():
-                    pass
-                else:
+                if not Authors.objects.filter(date_of_birth=author_birth, name_and_surname=author_name_and_surname).exists():
                     Authors.objects.create(
                         date_of_birth=author_birth,
                         name_and_surname=author_name_and_surname,
                         )
-                if Books.objects.filter(year_of_writing=year_of_writing, name=name_of_book).exists():
-                    pass
-                else:
+                if not Books.objects.filter(year_of_writing=year_of_writing, name=name_of_book).exists():
                     Books.objects.create(
                         year_of_writing=year_of_writing,
                         name=name_of_book,
@@ -45,7 +41,9 @@ def upload_file(request):
         obj.activated = True
         obj.save()
         return redirect('/')
+
     context = {
         'form': form,
+        'information': 'Accepted only CSV files',
     }
     return render(request, 'upload_file.html', context)
